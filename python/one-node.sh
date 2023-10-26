462e6e9d9ba9e16eb327c405cd2af463fdf8661d
Ratpenk40 <alexandre.vaudano@gmail.com>
2023-10-26 21:18:29 +0200

    Cmake mex6

--- python/one-node.sh
+++ python/one-node.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task 1             # number of cpus for each task. One by default
 #SBATCH --partition public-cpu         # the partition to use. By default debug-cpu
 #SBATCH --mem-per-cpu=2000             #in MB
-#SBATCH --time 08:00:00                  # maximum run time.
+#SBATCH --time 12:00:00                  # maximum run time.
  
 module load foss/2019b ROOT Boost/1.71.0 Boost.Python/1.71.0 matplotlib/3.1.1-Python-3.7.4   # load a specific software using module, for example Python
  
-srun python main.py -p 100000 --bound --plk1 --threeD --slice --settings settingsTemplate.txt  # run your software 
+srun python main.py -p 100000 --bound --mex6 --plk1 --threeD --slice --settings settingsTemplate.txt  # run your software 
